8d66361d04af2f0bcc337b38c1cce2cd8094dd4b
David Konsumer <konsumer@jetboystudio.com>
2021-07-27 16:17:13 -0700

    intiial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# node GK6X
+
+This adds support for GK6X keyboards in nodejs.
+
+--- 
+
+# WIP
+
+This is a work in progress. It's not ready for use.
+
+---
+
+This will allow you program a ton of custom configs, in Mac, Windows, and Linux.
+
+## supported devices
+
+On linux, you can see if your keyboard is supported by typing this:
+
+```sh
+lsusb
+```
+
+you should see a line that looks like this:
+
+```
+Bus 001 Device 016: ID 1ea7:0907 SHARKOON Technologies GmbH Keyboard
+```
+
+I'm not quite sure how to do this on other OS's (someone that uses them should let me know!)
+
+## usage
+
+### CLI
+
+> explanation goes here
+
+### library
+
+You can also use this as a library in your own node-based project like this:
+
+```sh
+npm i gk6x
+```
+
+> explanation goes here
+
+## thanks
+
+This is all due to [the awesome work of @pixeltris](https://github.com/pixeltris/GK6X) in C#. They, in-turn, used [@wgwoods work in python](https://github.com/wgwoods/gk64-python).
---
+++ index.js
@@ -0,0 +1,49 @@
+import { devices, HID, setDriverType } from 'node-hid'
+import * as opcodes from './opcodes'
+
+// List all devices that could be a gk6x
+// this is probly not really going to be used
+export const list = () => devices().filter(d => d.vendorId === 7847 && d.productId === 2311)
+
+// porting form this: https://github.com/pixeltris/GK6X/blob/899885b5dd27e5a734806378bdaa344dfac1b65f/GK6X/KeyboardDevice.cs#L353
+export class Gk6x {
+  constructor () {
+    this.device = new HID(7847, 2311)
+  }
+
+  write (op1, op2, packet, op3 = 0) {
+    // TODO: look in to what node-hid feature-reports are, they might be better here
+    return this.device.write([op1, op2, packet, op3])
+  }
+
+  setLayer (layer) {
+
+  }
+
+  setLighting (layer, userData) {
+
+  }
+
+  setMacros (layer, userData) {
+
+  }
+
+  setKeys (layer, values, fnPressed) {
+
+  }
+
+  setIdentifyDriverMacros () {
+
+  }
+}
+
+// testing
+
+const keyboards = list()
+let currentKeyboard = keyboards.length-1
+console.log(keyboards[currentKeyboard])
+
+const device = new Gk6x()
+
+
+
